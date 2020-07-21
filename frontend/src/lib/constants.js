62a47b17a41883512d85424419f72677be45a702
강병국 <kang9305@naver.com>
2020-07-21 13:50:43 +0900

    first commit

---
+++ frontend/src/lib/constants.js
@@ -0,0 +1,21 @@
+export default {
+    TITLE: 'SSAFY 개발블로그',
+    baseUrl: 'http://127.0.0.1:8000/',
+    LS_KEY: {
+        USER_TOKEN: 'userToken',
+    },
+    ERROR: { 
+        FRONT_ERROR: 'FE00' 
+    },
+    URL_TYPE: {
+        USER: {
+            LOGIN: 'login',
+            JOIN: 'join',
+            LOGOUT : 'logout', 
+        },
+        POST: {
+            MAIN : "main",
+        },
+    }
+}
+
