005b98aa9e01a89ec236ddd374cb123123d3a385
kang9305 <kang9305@naver.com>
2020-08-15 22:26:55 +0900

    프로필 페이지 수정 시작

--- backend/frontend/src/page/user/profile/UserProfile.vue
+++ backend/frontend/src/page/user/profile/UserProfile.vue
@@ -1,16 +1,24 @@
 <template>
-  <div class="container mt-5">
+  <div class="user-profile">
     <div class="row">
       <div class="col-3" style="height:100vh">
         <div class="col-12">
           <router-link
-            :to="{name : constants.URL_TYPE.USER.PROFILECONFIG , params : { username : username}}"
-          >프로필 페이지</router-link>
+            :to="{
+              name: constants.URL_TYPE.USER.PROFILECONFIG,
+              params: { username: username },
+            }"
+            >프로필 페이지</router-link
+          >
         </div>
         <div class="col-12 mt-5">
           <router-link
-            :to="{name : constants.URL_TYPE.USER.RECEIVECONFIRM , params : { username : username}}"
-          >받은 수정요청</router-link>
+            :to="{
+              name: constants.URL_TYPE.USER.RECEIVECONFIRM,
+              params: { username: username },
+            }"
+            >받은 수정요청</router-link
+          >
         </div>
         <div class="col-12 mt-5">보낸 수정요청</div>
       </div>
@@ -24,6 +32,7 @@
 <script>
 import constants from "../../../lib/constants";
 import { mapState } from "vuex";
+import "../../../assets/css/user.scss";
 export default {
   name: "profile",
   data() {
@@ -37,5 +46,4 @@ export default {
 };
 </script>
 
-<style>
-</style>
+<style></style>
