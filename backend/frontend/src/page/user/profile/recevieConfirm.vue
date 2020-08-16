501ed24997e9152998c392a279996a8e59c6a469
kang9305 <kang9305@naver.com>
2020-08-17 00:37:31 +0900

    수정 요청 table 작업중

--- backend/frontend/src/page/user/profile/recevieConfirm.vue
+++ backend/frontend/src/page/user/profile/recevieConfirm.vue
@@ -1,7 +1,20 @@
 <template>
-  <div>
-    <div>
-      <h1>받은 수정요청입니다.</h1>
+  <div class="update-request">
+    <div class="table">
+      <tr>
+        <th>No.</th>
+        <th>수정 요청 글</th>
+        <th>요청한 유저</th>
+        <th>요청일</th>
+        <th>처리 상태</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>welog 잘만들었다</td>
+        <td>김영환</td>
+        <td>2020-08-15</td>
+        <td>reject</td>
+      </tr>
     </div>
   </div>
 </template>
