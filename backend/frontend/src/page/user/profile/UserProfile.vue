b846692a53dd1533b916b7b5e1d58cf2dfcace8a
Young Hwan Kim <aorkslrdnjwm@naver.com>
2020-08-18 15:55:21 +0900

    프로필 페이지 반응형 + 다른페이지들도 반응형 조금 수정

--- backend/frontend/src/page/user/profile/UserProfile.vue
+++ backend/frontend/src/page/user/profile/UserProfile.vue
@@ -8,8 +8,9 @@
               name: constants.URL_TYPE.USER.PROFILECONFIG,
               params: { username: username },
             }"
-            ><i class="fas fa-cog"></i> 프로필 설정</router-link
           >
+            <i class="fas fa-cog"></i> 프로필 설정
+          </router-link>
         </div>
         <div class="side-item mt-5">
           <router-link
@@ -17,13 +18,43 @@
               name: constants.URL_TYPE.USER.RECEIVECONFIRM,
               params: { username: username },
             }"
-            ><i class="fas fa-exclamation-circle"></i> 받은
-            수정요청</router-link
           >
+            <i class="fas fa-exclamation-circle"></i> 받은
+            수정요청
+          </router-link>
         </div>
         <div class="side-item mt-5">
           <i class="fas fa-paper-plane"></i>보낸 수정요청
         </div>
+      </div>
+      <!-- 모바일 사이즈에서 보이는 사이드(?)바 -->
+      <div class="small-side-bar">
+        <div class="small-side-item">
+          <router-link
+            :to="{
+              name: constants.URL_TYPE.USER.PROFILECONFIG,
+              params: { username: username },
+            }"
+          >
+            <i class="fas fa-cog"></i>
+          </router-link>
+        </div>
+        <div class="small-side-item">
+          <router-link
+            :to="{
+              name: constants.URL_TYPE.USER.RECEIVECONFIRM,
+              params: { username: username },
+            }"
+          >
+            <i class="fas fa-exclamation-circle"></i>
+          </router-link>
+        </div>
+        <div class="small-side-item">
+          <i class="fas fa-paper-plane"></i>
+        </div>
+      </div>
+      <div class="profile-view">
+        <router-view />
       </div>
       <div class="profile-view">
         <router-view />
